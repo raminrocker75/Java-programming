6cc7d58a0b2abb30a4f4b655ea05a0885ac95872
Ramin <raminrocker75@gmail.com>
2021-05-27 19:22:30 -0500

    readAddress

---
+++ src/Day_47_Constructors/ReadAddress.java
@@ -0,0 +1,13 @@
+package Day_47_Constructors;
+
+public class ReadAddress {
+    public static void main(String[] args) {
+        Address address = new Address();
+        address.setStreet("12617 Sauterne drive");
+        address.setCity("Saint Louis");
+        address.setState("Mo");
+        address.setZipcode("63146");
+
+        System.out.println(address.getStreet()+ " " +address.getCity()+" " +address.getState()+" "+address.getZipcode()+". "+address.getCountry());
+    }
+}
