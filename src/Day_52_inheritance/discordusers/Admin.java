1c7d8bfe948e0fdcb95c32abe23111e33f546355
Ramin <raminrocker75@gmail.com>
2021-06-12 16:11:44 -0500

    added new commit

--- src/Day_52_inheritance/discordusers/Admin.java
+++ src/Day_52_inheritance/discordusers/Admin.java
@@ -3,6 +3,13 @@ package Day_52_inheritance.discordusers;
 public class Admin extends User{
 
     public Admin() {
+        super(); //call super class/User class no-args constructor
+        System.out.println("Admin class constructor");
+    }
+
+    public Admin(String name, int id) {
+        super("Admin", name, id);//public User(String role, String name, int id)
+        System.out.println("Admin class 2 args constructor");
     }
 
     public String toString(){
--- src/Day_52_inheritance/discordusers/User.java
+++ src/Day_52_inheritance/discordusers/User.java
@@ -5,9 +5,17 @@ public class User {
     private String name;
     private int id;
 
-    public  User(){
+    public User() {
         System.out.println("User class constructor");
     }
+
+    public User(String role, String name, int id) {
+        System.out.println("User class 3 args constructor");
+        this.role = role;
+        this.name = name;
+        this.id = id;
+    }
+
 
     @Override
     public String toString() {
