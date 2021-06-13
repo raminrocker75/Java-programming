146bb684dfce86877fcdc639038f8130265d2dde
Ramin <raminrocker75@gmail.com>
2021-06-13 09:41:42 -0500

    day_53 commit

---
+++ src/Day_53_inheritance/ElectricCar.java
@@ -0,0 +1,62 @@
+package Day_53_inheritance;
+
+public class ElectricCar {
+    private String model;
+    private String make;
+    private double price;
+    private int year;
+    private int range;
+
+    public ElectricCar(int range) {
+        this.range = range;
+    }
+
+    protected void drive(int miles){
+        if (range==0 || range - miles < 0){
+            System.out.println("Error: Cannot drive that far, need to charge.");
+        }else {
+            range -= miles;
+            System.out.println("Driving " + miles + " miles....");
+        }
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getRange() {
+        return range;
+    }
+
+    public void setRange(int range) {
+        this.range = range;
+    }
+}
