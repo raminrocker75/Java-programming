08d14880059134aaf7602004b1f545e38f2251d3
Ramin <raminrocker75@gmail.com>
2021-06-14 17:16:54 -0500

    new commit

---
+++ src/OfficeHours/Practice_06_14_2021/Take5.java
@@ -0,0 +1,17 @@
+package OfficeHours.Practice_06_14_2021;
+/*
+Create a class Take5 [Child class]
+
+    * Inherits Candy class
+
+    * constructor that invokes the parent constructor
+
+    * extra: static hiding, final
+
+ */
+public class Take5 extends Candy{
+    public Take5(int quantity, boolean containsPeanuts) {
+        super("Reese", quantity, containsPeanuts);
+    }
+
+}
